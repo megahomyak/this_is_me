8f1f833e2bf2e211cb8b67649a026a70be0014f3
megahomyak <g.megahomyak@gmail.com>
2024-11-16 04:32:47 +0500

    Decent-ish arms

--- script.py
+++ script.py
@@ -6,6 +6,7 @@ from mediapipe.python.solutions import pose as mediapipe_pose
 from mediapipe.framework.formats.landmark_pb2 import NormalizedLandmark
 from pyvirtualcam import Camera
 from copy import copy
+from dataclasses import dataclass
 
 'LEFT_ANKLE', 'LEFT_EAR', 'LEFT_ELBOW', 'LEFT_EYE', 'LEFT_EYE_INNER', 'LEFT_EYE_OUTER', 'LEFT_FOOT_INDEX', 'LEFT_HEEL', 'LEFT_HIP', 'LEFT_INDEX', 'LEFT_KNEE', 'LEFT_PINKY', 'LEFT_SHOULDER', 'LEFT_THUMB', 'LEFT_WRIST', 'MOUTH_LEFT', 'MOUTH_RIGHT', 'NOSE', 'RIGHT_ANKLE', 'RIGHT_EAR', 'RIGHT_ELBOW', 'RIGHT_EYE', 'RIGHT_EYE_INNER', 'RIGHT_EYE_OUTER', 'RIGHT_FOOT_INDEX', 'RIGHT_HEEL', 'RIGHT_HIP', 'RIGHT_INDEX', 'RIGHT_KNEE', 'RIGHT_PINKY', 'RIGHT_SHOULDER', 'RIGHT_THUMB', 'RIGHT_WRIST'
 
@@ -26,6 +27,7 @@ MOUTH_COLOR = make_color(0, 0, 0)
 EYE_COLOR = make_color(0, 217, 232)
 HAIR_COLOR = make_color(109, 71, 0)
 SWEATSHIRT_BASE_COLOR = make_color(0, 170, 0)
+SWEATSHIRT_STRIPE_COLOR = make_color(255, 255, 255)
 
 SHOW_AUDIO_DEVICES = False
 
@@ -47,12 +49,13 @@ def process_sound(indata, _frames, _time, _status):
     if mic_volume < AUDIO_THRESHOLD:
         mic_volume = 0
 
+@dataclass
+class Point:
+    x: float
+    y: float
+
 def middle(mark1, mark2):
-    args = {
-        attr: (getattr(mark1, attr) + getattr(mark2, attr)) / 2
-        for attr in ["x", "y", "z"]
-    }
-    return NormalizedLandmark(**args)
+    return Point((mark1.x + mark2.x) / 2, (mark1.y + mark2.y) / 2)
 
 if SHOW_AUDIO_DEVICES:
     devices = sounddevice.query_devices()
@@ -102,25 +105,21 @@ def start():
                 p2 = (center.x + width/2, center.y + height/2)
                 draw_rectangle_low_level(p1, p2, color)
 
-            def draw_chain_middle_link(beginning, end, width, height, base_color, final_color, recursion_level):
-                middle_link = middle(beginning, end)
-                if recursion_level == 0:
-                    base_color = final_color
-                draw_rectangle(middle_link, width, height, base_color)
-                if recursion_level != 0:
-                    recursion_level -= 1
-                    draw_chain_middle_link(beginning, middle, width, height, base_color, final_color, recursion_level)
-                    draw_chain_middle_link(middle, end, width, height, base_color, final_color, recursion_level)
-
-            def draw_chain(beginning, end, width, height, base_color, final_color, recursion_level):
-                draw_rectangle(beginning, width, height, base_color)
-                draw_chain_middle_link(beginning, end, width, height, color, recursion_level)
-                draw_rectangle(end, width, height, base_color)
+            def draw_chain(beginning, end, width, height, colors_list):
+                for i in range(len(colors_list)):
+                    color = colors_list[i]
+                    progression = i/(len(colors_list)-1)
+                    midpoint = Point(
+                        x=(1-progression)*beginning.x + progression*end.x,
+                        y=(1-progression)*beginning.y + progression*end.y,
+                    )
+                    draw_rectangle(midpoint, width, height, color)
 
             pose = pose_recognizer.process(input_frame)
 
             def get_landmark(landmark_name):
-                return pose.pose_landmarks.landmark[getattr(mediapipe_pose.PoseLandmark, landmark_name)]
+                landmark = pose.pose_landmarks.landmark[getattr(mediapipe_pose.PoseLandmark, landmark_name)]
+                return Point(landmark.x, landmark.y)
 
             if pose.pose_landmarks is not None:
                 head_center = middle(get_landmark("RIGHT_EAR"), get_landmark("LEFT_EAR"))
@@ -146,6 +145,13 @@ def start():
                 draw_rectangle_low_level((closed_mouth_center.x - 1.5*PIXEL_SIZE, closed_mouth_center.y), (closed_mouth_center.x + 1.5*PIXEL_SIZE, closed_mouth_center.y + mouth_length), MOUTH_COLOR)
                 # Nose
                 draw_rectangle(nose_center, 1, 1, NOSE_COLOR)
+                # Arms
+                for shoulder, elbow in (("LEFT_SHOULDER", "LEFT_ELBOW"), ("RIGHT_SHOULDER", "RIGHT_ELBOW")):
+                    shoulder = get_landmark(shoulder)
+                    elbow = get_landmark(elbow)
+                    elbow.y += PIXEL_SIZE
+                    shoulder.y += PIXEL_SIZE
+                    draw_chain(shoulder, elbow, 2, 4, [SWEATSHIRT_BASE_COLOR, SWEATSHIRT_STRIPE_COLOR, SWEATSHIRT_BASE_COLOR, SWEATSHIRT_STRIPE_COLOR, SWEATSHIRT_BASE_COLOR])
 
             camera.send(output_frame)
 
