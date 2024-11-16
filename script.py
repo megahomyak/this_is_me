9008ddc3128ef9a285092bfd647809c8eb723f0c
megahomyak <g.megahomyak@gmail.com>
2024-11-16 06:09:38 +0500

    A bunch of shit tweaked + body

--- script.py
+++ script.py
@@ -7,6 +7,7 @@ from mediapipe.framework.formats.landmark_pb2 import NormalizedLandmark
 from pyvirtualcam import Camera
 from copy import copy
 from dataclasses import dataclass
+import random
 
 'LEFT_ANKLE', 'LEFT_EAR', 'LEFT_ELBOW', 'LEFT_EYE', 'LEFT_EYE_INNER', 'LEFT_EYE_OUTER', 'LEFT_FOOT_INDEX', 'LEFT_HEEL', 'LEFT_HIP', 'LEFT_INDEX', 'LEFT_KNEE', 'LEFT_PINKY', 'LEFT_SHOULDER', 'LEFT_THUMB', 'LEFT_WRIST', 'MOUTH_LEFT', 'MOUTH_RIGHT', 'NOSE', 'RIGHT_ANKLE', 'RIGHT_EAR', 'RIGHT_ELBOW', 'RIGHT_EYE', 'RIGHT_EYE_INNER', 'RIGHT_EYE_OUTER', 'RIGHT_FOOT_INDEX', 'RIGHT_HEEL', 'RIGHT_HIP', 'RIGHT_INDEX', 'RIGHT_KNEE', 'RIGHT_PINKY', 'RIGHT_SHOULDER', 'RIGHT_THUMB', 'RIGHT_WRIST'
 
@@ -25,9 +26,15 @@ FACE_COLOR = make_color(245, 196, 129)
 NOSE_COLOR = make_color(198, 158, 104)
 MOUTH_COLOR = make_color(0, 0, 0)
 EYE_COLOR = make_color(0, 217, 232)
+CLOSED_EYE_COLOR = make_color(0, 0, 0)
 HAIR_COLOR = make_color(109, 71, 0)
 SWEATSHIRT_BASE_COLOR = make_color(0, 170, 0)
 SWEATSHIRT_STRIPE_COLOR = make_color(255, 255, 255)
+HAND_COLOR = make_color(225, 176, 109)
+
+MIN_BLINKING_RATE = 20
+MAX_BLINKING_RATE = 30
+blink_stage = 0
 
 SHOW_AUDIO_DEVICES = False
 
@@ -54,8 +61,9 @@ class Point:
     x: float
     y: float
 
-def middle(mark1, mark2):
-    return Point((mark1.x + mark2.x) / 2, (mark1.y + mark2.y) / 2)
+def average(*points):
+    avg = lambda attribute: sum(map(lambda point: getattr(point, attribute), points)) / len(points)
+    return Point(x=avg("x"), y=avg("y"))
 
 if SHOW_AUDIO_DEVICES:
     devices = sounddevice.query_devices()
@@ -71,9 +79,10 @@ def start():
     video_capture = cv2.VideoCapture(0)
     input_to_output_flash_stage = 0
     input_to_output_flash_shown = True
+    current_pose = None
     with \
         sounddevice.InputStream(device=MIC_DEVICE, callback=process_sound, latency="low"), \
-        mediapipe_pose.Pose() as pose_recognizer, \
+        mediapipe_pose.Pose(min_detection_confidence=0.9, min_tracking_confidence=0.5, model_complexity=0) as pose_recognizer, \
         Camera(width=OUTPUT_WIDTH, height=OUTPUT_HEIGHT, fps=int(video_capture.get(cv2.CAP_PROP_FPS)) * 2, backend=CAMERA_BACKEND) as camera:
         while video_capture.isOpened():
             is_success, input_frame = video_capture.read()
@@ -92,10 +101,10 @@ def start():
             def draw_rectangle_low_level(beginning, end, color):
                 if not input_to_output_flash_shown:
                     return
-                bottom_row = clamp(0, int(end.y*OUTPUT_HEIGHT), OUTPUT_HEIGHT - 1)
-                top_row = clamp(0, int(beginning.y*OUTPUT_HEIGHT), OUTPUT_HEIGHT - 1)
-                left_column = clamp(0, int(beginning.x*OUTPUT_WIDTH), OUTPUT_WIDTH - 1)
-                right_column = clamp(0, int(end.x*OUTPUT_WIDTH), OUTPUT_WIDTH - 1)
+                bottom_row = clamp(0, int(end.y*OUTPUT_HEIGHT), OUTPUT_HEIGHT)
+                top_row = clamp(0, int(beginning.y*OUTPUT_HEIGHT), OUTPUT_HEIGHT)
+                left_column = clamp(0, int(beginning.x*OUTPUT_WIDTH), OUTPUT_WIDTH)
+                right_column = clamp(0, int(end.x*OUTPUT_WIDTH), OUTPUT_WIDTH)
                 output_frame[top_row:bottom_row, left_column:right_column] = color
 
             def draw_rectangle(center, width, height, color):
@@ -115,22 +124,19 @@ def start():
                     )
                     draw_rectangle(midpoint, width, height, color)
 
-            pose = pose_recognizer.process(input_frame)
+            new_pose = pose_recognizer.process(input_frame)
+
+            if new_pose.pose_landmarks is not None:
+                current_pose = new_pose
 
             def get_landmark(landmark_name):
-                landmark = pose.pose_landmarks.landmark[getattr(mediapipe_pose.PoseLandmark, landmark_name)]
+                landmark = current_pose.pose_landmarks.landmark[getattr(mediapipe_pose.PoseLandmark, landmark_name)]
                 return Point(landmark.x, landmark.y)
 
-            if pose.pose_landmarks is not None:
-                head_center = middle(get_landmark("RIGHT_EAR"), get_landmark("LEFT_EAR"))
+            if current_pose is not None:
+                head_center = average(get_landmark("RIGHT_EAR"), get_landmark("LEFT_EAR"))
                 nose_center = get_landmark("NOSE")
-                face_center = middle(head_center, nose_center)
-                left_eye_center = copy(face_center)
-                left_eye_center.x -= PIXEL_SIZE
-                left_eye_center.y -= PIXEL_SIZE
-                right_eye_center = copy(face_center)
-                right_eye_center.x += PIXEL_SIZE
-                right_eye_center.y -= PIXEL_SIZE
+                face_center = average(head_center, nose_center)
                 closed_mouth_center = copy(face_center)
                 closed_mouth_center.y += PIXEL_SIZE * 1.5
                 # Face
@@ -141,33 +147,52 @@ def start():
                 hair_top_left.x -= 2.5 * PIXEL_SIZE
                 hair_bottom_right = copy(hair_top_left)
                 hair_bottom_right.x += 5 * PIXEL_SIZE
-                hair_bottom_right.y += 2 * PIXEL_SIZE
+                hair_bottom_right.y = face_center.y - 1.5 * PIXEL_SIZE
                 draw_rectangle_low_level(hair_top_left, hair_bottom_right, HAIR_COLOR)
                 # Hair cut
                 hair_cut_top_left = copy(hair_top_left)
                 hair_cut_top_left.x = clamp(hair_top_left.x, face_center.x + 0.5 * PIXEL_SIZE, hair_bottom_right.x)
-                hair_cut_top_left.y += 1 * PIXEL_SIZE
+                hair_cut_top_left.y = hair_bottom_right.y - 1*PIXEL_SIZE
                 hair_cut_bottom_right = copy(hair_cut_top_left)
                 hair_cut_bottom_right.x += 1 * PIXEL_SIZE
-                hair_cut_bottom_right.y += 1 * PIXEL_SIZE
+                hair_cut_bottom_right.y = hair_bottom_right.y
                 draw_rectangle_low_level(hair_cut_top_left, hair_cut_bottom_right, FACE_COLOR)
-                # Left eye
-                draw_rectangle(left_eye_center, 1, 1, EYE_COLOR)
-                # Right eye
-                draw_rectangle(right_eye_center, 1, 1, EYE_COLOR)
+                # Eyes
+                for eye_shift in (PIXEL_SIZE, -PIXEL_SIZE):
+                    eye_center = copy(face_center)
+                    eye_center.x += eye_shift
+                    global blink_stage
+                    if blink_stage == 0:
+                        eye_center.y -= PIXEL_SIZE * 0.8
+                        draw_rectangle(eye_center, 1, 1/8, CLOSED_EYE_COLOR)
+                    else:
+                        eye_center.y -= PIXEL_SIZE
+                        draw_rectangle(eye_center, 1, 1, EYE_COLOR)
+                if blink_stage == 0:
+                    blink_stage = random.randint(MIN_BLINKING_RATE, MAX_BLINKING_RATE)
+                else:
+                    blink_stage -= 1
+                # Mouth
                 mouth_length_bias = mic_volume*PIXEL_SIZE
                 mouth_length = BASE_MOUTH_LENGTH + mouth_length_bias
-                # Mouth
                 draw_rectangle_low_level(Point(closed_mouth_center.x - 1.5*PIXEL_SIZE, closed_mouth_center.y), Point(closed_mouth_center.x + 1.5*PIXEL_SIZE, closed_mouth_center.y + mouth_length), MOUTH_COLOR)
                 # Nose
                 draw_rectangle(nose_center, 1, 1, NOSE_COLOR)
+                # Body
+                midshoulders = average(get_landmark("LEFT_SHOULDER"), get_landmark("RIGHT_SHOULDER"))
+                midhips = average(get_landmark("LEFT_HIP"), get_landmark("RIGHT_HIP"))
+                body_width = abs(get_landmark("LEFT_SHOULDER").x - get_landmark("RIGHT_SHOULDER").x)
+                draw_chain(midshoulders, midhips, body_width/PIXEL_SIZE, 2, [*[SWEATSHIRT_BASE_COLOR, SWEATSHIRT_STRIPE_COLOR]*4, SWEATSHIRT_BASE_COLOR])
                 # Arms
-                for shoulder, elbow in (("LEFT_SHOULDER", "LEFT_ELBOW"), ("RIGHT_SHOULDER", "RIGHT_ELBOW")):
-                    shoulder = get_landmark(shoulder)
-                    elbow = get_landmark(elbow)
-                    elbow.y += PIXEL_SIZE
-                    shoulder.y += PIXEL_SIZE
-                    draw_chain(shoulder, elbow, 2, 4, [SWEATSHIRT_BASE_COLOR, SWEATSHIRT_STRIPE_COLOR, SWEATSHIRT_BASE_COLOR, SWEATSHIRT_STRIPE_COLOR, SWEATSHIRT_BASE_COLOR])
+                for beginning, end in (("LEFT_SHOULDER", "LEFT_ELBOW"), ("LEFT_ELBOW", "LEFT_WRIST"), ("RIGHT_SHOULDER", "RIGHT_ELBOW"), ("RIGHT_ELBOW", "RIGHT_WRIST")):
+                    beginning = get_landmark(beginning)
+                    end = get_landmark(end)
+                    end.y += PIXEL_SIZE
+                    beginning.y += PIXEL_SIZE
+                    draw_chain(beginning, end, 2, 4, [*[SWEATSHIRT_BASE_COLOR, SWEATSHIRT_STRIPE_COLOR]*2, SWEATSHIRT_BASE_COLOR])
+                for hand in ("RIGHT", "LEFT"):
+                    hand_center = average(*(get_landmark(f"{hand}_{part}") for part in ("WRIST", "PINKY", "INDEX")))
+                    draw_rectangle(hand_center, 3, 3, HAND_COLOR)
 
             camera.send(output_frame)
 
