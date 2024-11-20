c01d784b254a9d571c51acb9b3a18fec382f93e0
megahomyak <g.megahomyak@gmail.com>
2024-11-20 23:01:21 +0500

    Added lower body

--- script.py
+++ script.py
@@ -11,9 +11,9 @@ import random
 
 'LEFT_ANKLE', 'LEFT_EAR', 'LEFT_ELBOW', 'LEFT_EYE', 'LEFT_EYE_INNER', 'LEFT_EYE_OUTER', 'LEFT_FOOT_INDEX', 'LEFT_HEEL', 'LEFT_HIP', 'LEFT_INDEX', 'LEFT_KNEE', 'LEFT_PINKY', 'LEFT_SHOULDER', 'LEFT_THUMB', 'LEFT_WRIST', 'MOUTH_LEFT', 'MOUTH_RIGHT', 'NOSE', 'RIGHT_ANKLE', 'RIGHT_EAR', 'RIGHT_ELBOW', 'RIGHT_EYE', 'RIGHT_EYE_INNER', 'RIGHT_EYE_OUTER', 'RIGHT_FOOT_INDEX', 'RIGHT_HEEL', 'RIGHT_HIP', 'RIGHT_INDEX', 'RIGHT_KNEE', 'RIGHT_PINKY', 'RIGHT_SHOULDER', 'RIGHT_THUMB', 'RIGHT_WRIST'
 
-PIXEL_SIZE = 0.07
+PIXEL_SIZE = 0.02
 
-MIC_DEVICE = "HDA Intel PCH: ALC255 Analog (hw:1,0)"
+MIC_DEVICE = ""
 MIC_SENSITIVITY = 500
 AUDIO_THRESHOLD = 0.5
 BASE_MOUTH_LENGTH = 1 * PIXEL_SIZE
@@ -31,6 +31,8 @@ HAIR_COLOR = make_color(109, 71, 0)
 SWEATSHIRT_BASE_COLOR = make_color(0, 170, 0)
 SWEATSHIRT_STRIPE_COLOR = make_color(255, 255, 255)
 HAND_COLOR = make_color(225, 176, 109)
+PANTS_COLOR = make_color(4, 71, 138)
+SHOE_COLOR = make_color(0, 0, 0)
 
 MIN_BLINKING_RATE = 20
 MAX_BLINKING_RATE = 30
@@ -68,8 +70,10 @@ def average(*points):
 if SHOW_AUDIO_DEVICES:
     devices = sounddevice.query_devices()
     for device in devices:
+        marker = " "
         if device["max_input_channels"] > 0:
-            print(f"Index: {device['index']}, name: \"{device['name']}\"")
+            marker = "X"
+        print(f"[{marker}] Index: {device['index']}, name: \"{device['name']}\"")
     exit()
 
 def clamp(bound1, value, bound2):
@@ -81,8 +85,7 @@ def start():
     input_to_output_flash_shown = True
     current_pose = None
     with \
-        sounddevice.InputStream(device=MIC_DEVICE, callback=process_sound, latency="low"), \
-        mediapipe_pose.Pose(min_detection_confidence=0.9, min_tracking_confidence=0.5, model_complexity=0) as pose_recognizer, \
+        mediapipe_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5, model_complexity=0) as pose_recognizer, \
         Camera(width=OUTPUT_WIDTH, height=OUTPUT_HEIGHT, fps=int(video_capture.get(cv2.CAP_PROP_FPS)) * 2, backend=CAMERA_BACKEND) as camera:
         while video_capture.isOpened():
             is_success, input_frame = video_capture.read()
@@ -114,15 +117,20 @@ def start():
                 end = Point(center.x + width/2, center.y + height/2)
                 draw_rectangle_low_level(beginning, end, color)
 
-            def draw_chain(beginning, end, width, height, colors_list):
-                for i in range(len(colors_list)):
-                    color = colors_list[i]
-                    progression = i/(len(colors_list)-1)
+            def get_chain(beginning, end, amount):
+                for i in range(amount):
+                    progression = i/(amount-1)
                     midpoint = Point(
                         x=(1-progression)*beginning.x + progression*end.x,
                         y=(1-progression)*beginning.y + progression*end.y,
                     )
-                    draw_rectangle(midpoint, width, height, color)
+                    yield midpoint
+
+            def draw_chain(beginning, end, width, height, colors_list):
+                for point, color in zip(get_chain(beginning, end, len(colors_list)), colors_list):
+                    if color is None:
+                        continue
+                    draw_rectangle(point, width, height, color)
 
             new_pose = pose_recognizer.process(input_frame)
 
@@ -141,9 +149,22 @@ def start():
                 midshoulders = average(get_landmark("LEFT_SHOULDER"), get_landmark("RIGHT_SHOULDER"))
                 midmouth = average(get_landmark("MOUTH_RIGHT"), get_landmark("MOUTH_LEFT"))
                 draw_rectangle_low_level(Point(midshoulders.x - 1.5*PIXEL_SIZE, head_center.y + 3.5*PIXEL_SIZE), Point(midshoulders.x + 1.5*PIXEL_SIZE, midshoulders.y), FACE_COLOR)
+                # Pants
+                left_pant_hip = get_landmark("LEFT_HIP")
+                left_pant_hip.y += PIXEL_SIZE * 2
+                right_pant_hip = get_landmark("RIGHT_HIP")
+                right_pant_hip.y += PIXEL_SIZE * 2
+                draw_chain(left_pant_hip, right_pant_hip, 4, 4, [None, *[PANTS_COLOR]*3, None])
+                for beginning, end in (("LEFT_HIP", "LEFT_KNEE"), ("LEFT_KNEE", "LEFT_HEEL"), ("RIGHT_HIP", "RIGHT_KNEE"), ("RIGHT_KNEE", "RIGHT_HEEL")):
+                    beginning = get_landmark(beginning)
+                    end = get_landmark(end)
+                    draw_chain(beginning, end, 3, 3, [PANTS_COLOR]*5)
+                # Shoes
+                for landmark_name in ("RIGHT_HEEL", "RIGHT_FOOT_INDEX", "LEFT_HEEL", "LEFT_FOOT_INDEX"):
+                    draw_rectangle(get_landmark(landmark_name), 4, 4, SHOE_COLOR)
                 # Body
                 midhips = average(get_landmark("LEFT_HIP"), get_landmark("RIGHT_HIP"))
-                body_width = abs(get_landmark("LEFT_SHOULDER").x - get_landmark("RIGHT_SHOULDER").x)
+                body_width = max(abs(get_landmark("LEFT_SHOULDER").x - get_landmark("RIGHT_SHOULDER").x), PIXEL_SIZE*3)
                 draw_chain(midshoulders, midhips, body_width/PIXEL_SIZE, 2, [*[SWEATSHIRT_BASE_COLOR, SWEATSHIRT_STRIPE_COLOR]*4, SWEATSHIRT_BASE_COLOR])
                 # Face
                 draw_rectangle(head_center, 5, 7, FACE_COLOR)
